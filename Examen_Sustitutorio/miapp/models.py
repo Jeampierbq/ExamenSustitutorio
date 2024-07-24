7601cc9e3affda36e5800961e859b17952d9e6bf
Jeampierbq <Jeampierbarrios04@gmail.com>
2024-07-24 10:28:37 -0500

    Barrios_ExamenSustitutorio_parte2_Bd

--- Examen_Sustitutorio/miapp/models.py
+++ Examen_Sustitutorio/miapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Barrios_Usuarios(models.Model):
+    DISPONIBILIDAD = [
+        ('A', 'Activado'),
+        ('D', 'Desactivado'),
+    ]
+    
+    Barrios_login = models.CharField(max_length=100)
+    Barrios_clave = models.CharField(max_length=100)
+    Barrios_disponible = models.CharField(max_length=1, choices=DISPONIBILIDAD, default='A')
+    Barrios_fecha_de_registro = models.DateTimeField(auto_now_add=True)
